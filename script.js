85fb55fef7aeb0ae3e6c6e6aeec62d1b05a18c1c
Pader Xiong <paderx26@gmail.com>
2024-05-13 22:01:24 -0500

    added for loops for characters

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 0; i < count; "iteration") {
-  // Loop body
+for (let character = 0; character < count; character = character + 1) {
+  rows.push(character);
 }
+
+let result = "";
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+console.log(result);
