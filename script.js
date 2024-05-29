a601df81a9500df9435568237ef0702f8c1c282b
Pader Xiong <paderx26@gmail.com>
2024-05-28 21:58:07 -0500

    created inverted pyramid by using unshift array method

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function padRow(rowNumber, rowCount) {
 }
 
 // TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+for (let i = 1; i <= count; i++) {
+  rows.unshift(padRow(i, count));
+}
 
 /* while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
@@ -23,14 +23,6 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }
  */
-
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
-console.log(shifted);
 
 let result = "";
 
